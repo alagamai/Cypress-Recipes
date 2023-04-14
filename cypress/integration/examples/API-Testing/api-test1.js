38408fa19dc3e3a32d8b6e0c0869ba5cc2f17281
Alagamai <alagamai.n@gmail.com>
2023-04-14 13:34:02 +0530

    add api test file

---
+++ cypress/integration/examples/API-Testing/api-test1.js
@@ -0,0 +1,57 @@
+import { faker } from '@faker-js/faker'
+
+const END_PT = {
+    "USER_URL": "https://gorest.co.in/public/v2/users/",
+    "URL2": "https://gorest.co.in/public/v2/posts/",
+    "URL3": "https://pokeapi.co/api/v2/pokemon/",
+    "URL4": "https://gorest.co.in/public/v2/graphql",
+    "Bearer_Rest": "e2360bb230c2fc83af7451f8346d9fcdb699879e3a8f14462a2187f9d670675e",
+    "Bearer_Graph": "3df84f0ba9a2028aa6efe7519e3d73466cb301501858acabaad74119082f3b02"
+  }
+
+  
+describe('api testing', () => {
+    it.only('api post test', () => {
+        const randomIndex = Math.floor(Math.random() * 2); // generate a random integer between 0 and 1
+        const gender = randomIndex === 0 ? "Male" : "Female";
+        let name = faker.name.firstName()
+        cy.request({
+            method: 'POST',
+            url:  END_PT.USER_URL,
+            failOnStatusCode : false,
+            auth: {
+                'bearer':  END_PT.Bearer_Rest
+            },  
+            body: {
+                "name": name, 
+                "gender": gender,
+                "email": faker.internet.email(name),
+                "status": "active"
+            }
+        }).as('resp')
+
+        cy.get('@resp').then(response => {
+            cy.task("log", response.body)
+        })
+
+    })
+    it("api get test", () => {
+
+        cy.request({
+            method: 'GET', 
+            url: END_PT.USER_URL,
+            failOnStatusCode: false, 
+
+        }).as('resp')
+
+        cy.get('@resp').its('status').should('eq', 200)
+        cy.get('@resp').then(response => {
+            const data = response.body
+            cy.task("log", JSON.stringify(response.body))
+            cy.task("log", data[0].name);
+        })
+
+    })
+
+
+})
